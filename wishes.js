15b2de6a98522ea5a96628ced2c199e68954868a
trungld2 <trungld2@vng.com.vn>
2024-02-10 02:34:38 +0700

    Vietsub

---
+++ wishes.js
@@ -0,0 +1,201 @@
+var wishes = [
+  [
+    "Năm Giáp Thìn tới",
+    "Ai cũng giàu to",
+    "Sức khỏe chẳng lo",
+    "Buồn bực xếp xó",
+    "Khó khăn chuyện nhỏ",
+    "Việc chạy ro ro",
+    "Không còn nhăn nhó",
+    "Muốn gì được đó!"
+  ],
+  [
+    "Chúc bạn luôn hoan hỉ",
+    "Sức khỏe bền bỉ",
+    "Công danh hết ý",
+    "Tiền vào bạc tỉ",
+    "Tiền ra ri rỉ",
+    "Tình yêu thỏa chí",
+    "Vạn sự như ý",
+    "Luôn cười hí hí",
+    "Cung hỷ cung hỷ"
+  ],
+  [
+    "Đầu xuân năm mới an khang",
+    "Lộc về phú quý mênh mang khắp nhà",
+    "Chúc cho các chị đẹp ra",
+    "Các anh khỏe mạnh xông pha phố phường",
+    "Chúc cho các cháu dễ thương",
+    "Chúc cho tất cả bốn phương đều mừng",
+    "Xuân này có đĩa bánh chưng",
+    "Cùng mâm ngũ quả ta bưng mâm thờ!!"
+  ],
+  [
+    "Chúc nhau sống khỏe như trâu",
+    "Sống dai như đỉa, sống lâu như rùa",
+    "Tiền tài danh vọng từa lưa",
+    "Gia đình đầm ấm cho vừa lòng nhau"
+  ],
+  [
+    "Tui cũng xin chúc bà con",
+    "Đàn ông lủng lẳng hai hòn còn nguyên",
+    "Đàn bà thêm đẹp, nhiều duyên",
+    "Trẻ con nho nhỏ có tiền ăn chơi!"
+  ],
+  [
+    "Vui vẻ như chim sẻ",
+    "Mạnh khỏe như đại bàng",
+    "Giàu sang như chim phụng",
+    "Làm lụng như chim sâu",
+    "Sống lâu như đà điểu"
+  ],
+  [
+    "Năm mới: Đau đầu vì nhà giàu!",
+    "Mệt mỏi vì học giỏi!",
+    "Buồn phiền vì nhiều tiền!",
+    "Ngang trái vì xinh gái!",
+    "Tàn phai vì đẹp giai!",
+    "Và mất ngủ vì không có đối thủ!"
+  ],
+  [
+    "Mừng 2024",
+    "Phát tài phát lộc",
+    "Tiền vô xồng xộc",
+    "Tiền ra từ từ",
+    "Sức khỏe có dư",
+    "Công danh tấn tới",
+    "Tình duyên phơi phới",
+    "Hạnh phúc thăng hoa",
+    "Xin chúc mọi nhà",
+    "Một năm đại thắng."
+  ],
+  [
+    "Chúc mọi người",
+    "Tiền vào bạc tỉ",
+    "Tiền ra rỉ rỉ",
+    "Miệng cười hi hi",
+    "Vạn sự như ý",
+    "Cung hỷ, cung hỷ!"
+  ],
+  [
+    "2024 chúc các bạn",
+    "Có nhiều người để ý",
+    "Tỏ tình nhiều ý",
+    "Tiền nhiều nặng ký",
+    "Công việc như ý",
+    "Miệng cười hi hí",
+    "Sống lâu một tí."
+  ],
+  [
+    "Cung chúc đoàn viên",
+    "Vạn sự bình yên",
+    "Hạnh phúc vô biên",
+    "Vui vẻ triền miên",
+    "Kiếm được nhiều tiền",
+    "Sung sướng như tiên"
+  ],
+  [
+    "Mừng đoàn viên tới",
+    "Ngàn lần như ý",
+    "Vạn lần như mơ",
+    "Triệu sự bất ngờ",
+    "Tỷ lần hạnh phúc"
+  ],
+  [
+    "Kính chúc ông bà",
+    "Sống lâu sức khỏe",
+    "Trẻ mãi không già",
+    "Yêu thương thuận hòa",
+    "Cửa nhà sung túc",
+    "Hạnh phúc an khang."
+  ],
+  [
+    "Chúc Ngoại xuân này tuổi chín mươi",
+    "Bình an mạnh khỏe mãi bên đời",
+    "Tâm bền nhiệt huyết hoài trải",
+    "Dạ vững tinh thần Nghĩa chẳng vơi."
+  ],
+  [
+    "Ngoài kia hoa nở muôn loài",
+    "Chúc ông bà nội thọ ngoài một trăm",
+    "Dồi dào sức khỏe như mong",
+    "Để cho con cháu an lòng làm ăn."
+  ],
+  [
+    "Thương nội lắm cả đời tần tảo",
+    "Năm tháng dài cơm áo nuôi con",
+    "Đến nay sức khoẻ héo mòn",
+    "Chúc con kính cẩn lòng son gửi người."
+  ],
+  [
+    "Chúc năm mới",
+    "Công thành danh toại",
+    "Chúc năm mới",
+    "Trẻ mãi không già",
+    "Chúc năm mới",
+    "Phúc lộc trường tồn",
+    "Chúc năm mới",
+    "Tấn tài, tấn lộc."
+  ],
+  [
+    "Chúc mừng năm mới 2024",
+    "Khỏe hơn Phạm Văn Mách",
+    "Mạnh hơn Barack Obama",
+    "Giàu hơn Bill Gates",
+    "Quyến rũ hơn Don Juan",
+    "Bí mật hơn… Bin Laden",
+    "May mắn hơn Xuân tóc đỏ."
+  ],
+  [
+    "Chúc Tết đến trăm điều như ý",
+    "Mừng xuân sang vạn sự thành công."
+  ],
+  [
+    "Kính chúc ba mẹ",
+    "Sức khoẻ dồi dào",
+    "Đôi lứa yêu nhau",
+    "Càng thêm nồng ấm."
+  ],
+  [
+    "Cung chúc tân niên",
+    "Sức khỏe vô biên."
+  ],
+  [
+    "Chúc các bạn có nhiều người để ý",
+    "Tỏ tình nhiều ý",
+    "Tiền nhiều nặng ký",
+    "Công việc vừa ý",
+    "Miệng cười mắt ti hí",
+    "Sống Lâu Một tí."
+  ],
+  [
+    "Mừng năm mới 2024 phát tài phát lộc",
+    "Tiền vô xồng xộc, tiền ra từ từ",
+    "Sức khỏe có dư, công danh tấn tới",
+    "Tình duyên phơi phới, hạnh phúc thăng hoa",
+    "Xin chúc mọi nhà một năm đại thắng."
+  ],
+  [
+    "Phúc đem lễ nghĩa trong nhà thịnh",
+    "Lộc nảy vinh hoa phú quý xuân."
+  ],
+  [
+    "Tiễn Mão đi chúc xuân vui hạnh phúc",
+    "Đón Thìn về mừng Tết đạt thành công."
+  ]
+]
+
+function getWish() {
+ let wish = wishes[Math.floor(Math.random() * wishes.length)]
+ text = ""
+ for (let i = 0; i < wish.length; i++) {
+    text += wish[i] + "<br/>"
+
+ }
+ return text
+}
+function fillWish() {
+    document.getElementById("wish_content").innerHTML = getWish()
+}
+
+fillWish()
